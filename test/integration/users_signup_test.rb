e268af2f8ecc5ab29c3b7ee142e9bd52f435b3ce
Herve AMIOT <amiot_herve@hotmail.fr>
2015-03-12 14:35:18 +0100

    Exercises (chapter 7)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,25 +5,34 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	get signup_path
 
   	assert_no_difference "User.count" do
-  		post users_path, user: { name: "",
-  								email: "user@invalide",
-  								password: "foo",
-  								password_confirmation: "bar" }
+  		post users_path, user: { 	name: "",
+	  								email: "user@invalide",
+	  								password: "foo",
+	  								password_confirmation: "bar" }
   		end
 
   		assert_template 'users/new'
+      assert_select 'div#error_explanation'
+      assert_select 'div.alert-danger'
+      assert_select 'div#error_explanation li', 4
+      assert_select 'div#error_explanation li', /.*name.*blank.*/i
+      assert_select 'div#error_explanation li', /.*email.*invalid.*/i
+      assert_select 'div#error_explanation li', /.*password.*too short.*/i
+      assert_select 'div#error_explanation li', /.*password confirmation.*match.*/i
   	end
 
   	test "valid signup information" do
   	get signup_path
 
   	assert_difference "User.count", 1 do
-  		post_via_redirect users_path, user: { name: "Example User",
-  								email: "user@example.com",
-  								password: "password",
-  								password_confirmation: "password" }
+  		post_via_redirect users_path, user: { 	name: "Example User",
+				  								email: "user@example.com",
+				  								password: "password",
+				  								password_confirmation: "password" }
   		end
 
   		assert_template 'users/show'
+      assert_not flash.empty?
+      assert_select 'div.alert-success'
   	end
 end
